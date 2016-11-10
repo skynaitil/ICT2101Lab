9ec186e0141a10c06c1f101b4595b614243dfff7
LT Tang <skytanglitian@gmail.com>
2016-11-10 10:37:32 +0800

    trial

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,11 @@
 public class Calculator {
   public int evaluate(String expression) {
     int sum = 0;
+	int haha=0;
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
     return sum;
 	
-	int haha=0;
+	
   }
 }
