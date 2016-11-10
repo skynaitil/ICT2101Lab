7ba5cd4af20bdd5dd3877f9ee048db34962bcb1f
hweichou <weichou_93@hotmail.sg>
2016-11-10 10:46:07 +0800

    updates

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,7 @@
 public class Calculator {
   public int evaluate(String expression) {
     int sum = 0;
-	int haha=0;
+	int hehehe=0;
     for (String summand: expression.split("\\+"))
       sum += Integer.valueOf(summand);
     return sum;
